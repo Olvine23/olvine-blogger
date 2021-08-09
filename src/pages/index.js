64ea94d6221035c2c3a807584d0cec62b57fea25
Olvine23 <olivinegeorge@gmail.com>
2021-08-09 14:19:36 -0700

    working on the colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,13 +41,13 @@ const BlogIndex = ({ data, location }) => {
                 <header>
                   <h2>
                     <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
+                      <span style ={{color:"white"}} itemProp="headline">{title}</span>
                     </Link>
                   </h2>
                   <small>{post.frontmatter.date}</small>
                 </header>
                 <section>
-                  <p
+                  <p style ={{color:"#fff3"}}
                     dangerouslySetInnerHTML={{
                       __html: post.frontmatter.description || post.excerpt,
                     }}
